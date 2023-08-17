c71d69587ef71c7b12ce429fb00546a9d18a50d0
ShestakovK <142473859+ShestakovK@users.noreply.github.com>
2023-08-17 14:11:43 +0800

    Update login.php

--- login.php
+++ login.php
@@ -3,7 +3,7 @@
   require_once __DIR__.'/google-api-php-client-main/vendor/autoload.php';
   
   //Прописываем путь к файлу с ключом сервисного аккаунта
-  $googleAccountKeyFilePath = __DIR__.'/test-api-project-396201-ecb1fb9bf8bf.json';
+  $googleAccountKeyFilePath = __DIR__.'Сюда запишите путь к скачанному ключу';
   putenv('GOOGLE_APPLICATION_CREDENTIALS='.$googleAccountKeyFilePath);
 
   //Так НАДО, если сомневаешься, лезь в документацию https://developers.google.com/sheets/api/
@@ -11,9 +11,9 @@
   $client->useApplicationDefaultCredentials();
 
   //Область к которым будет доступ, весь перечень есть в документации во вкладке Начало работы -> Выберите области действия
-  $client->addScope('https://www.googleapis.com/auth/spreadsheets' );
-  $service = new Google_Service_Sheets( $client );
+  $client->addScope('https://www.googleapis.com/auth/spreadsheets');
+  $service = new Google_Service_Sheets($client);
 
   //ID таблицы
-  $spreadsheetId = '1KdTaK81c3BURTW4D7QMGgkEexd_ATYx3pUiUNbsmNlY';
+  $spreadsheetId = 'Сюда запишите ID таблицы';
 ?>
